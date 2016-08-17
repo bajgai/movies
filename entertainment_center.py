5894ba18b28aea330f9fb3e87efd98043ef71c77
niranjanbajgai <bajgainiranjan@gmail.com>
2016-08-17 11:06:25 -0600

    first commit

---
+++ entertainment_center.py
@@ -0,0 +1,111 @@
+import media
+import fresh_tomatoes
+
+inceptions =media.Movie("Inception", 
+"Mind bending story of on man's quest to steal a secret from dream",
+"http://t2.gstatic.com/images?q=tbn:ANd9GcRo9vfJCM6dzPkZHIHBVCtlJnAnew9Ai26kEdrli0-tfmatmciD",
+"https://www.youtube.com/watch?v=66TuSJo4dZM")
+
+avatar = media.Movie("Avatar", 
+"A marines advanture in a foreign planet",
+"https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+the_departed = media.Movie("The Departed", 
+"Story of an FBI agent who infeltrates a notorious boston gang",
+"https://upload.wikimedia.org/wikipedia/en/5/50/Departed234.jpg",
+"https://www.youtube.com/watch?v=n4O3x5BH18E")
+
+there_will_be_blood = media.Movie("There will be blood",
+"A story of family, religion, hatred, oil and madness, focusing on a turn-of-the-century prospector in the early days of the business.",
+"https://upload.wikimedia.org/wikipedia/en/d/da/There_Will_Be_Blood_Poster.jpg",
+"https://www.youtube.com/watch?v=FeSLPELpMeM")
+
+life_is_beautiful = media.Movie("Life is beautiful", 
+"A story of a dad who keeps his son alive in a concentration camp through humor and games",
+"https://upload.wikimedia.org/wikipedia/en/7/7c/Vitaebella.jpg",
+"https://www.youtube.com/watch?v=pAYEQP8gx3w")
+
+the_dark_knight = media.Movie("The Dark Knight",
+"Batman and his greatest adversery yet",
+"https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
+"https://www.youtube.com/watch?v=EXeTwQWrcwY")
+
+forest_gump =media.Movie("Forest Gump",
+"Advanture of a mentally disabled but kind harted war veteren as he nagivates through 1960's America",
+"https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
+"https://www.youtube.com/watch?v=uPIEn0M8su0")
+
+the_matrix = media.Movie("The Matrix",
+"A hero's quest to save humanity from robots who have inslaved humans by creating an artifical reality",
+"https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+"https://www.youtube.com/watch?v=vKQi3bBA1y8")
+
+american_history_x =media.Movie("American History X", 
+"A story of white supremacist in a modern world who ultimately changes his view",
+"https://upload.wikimedia.org/wikipedia/en/0/0a/American_history_x_poster.jpg",
+"https://www.youtube.com/watch?v=JsPW6Fj3BUI")
+
+intersteller = media.Movie("Intersteller", 
+"The earth is dyeing. A professor creates a wormhole for a team of astrounouts so that they can search for a new place for earth's inhabitants.",
+"https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+"https://www.youtube.com/watch?v=0vxOhd4qlnA")
+
+the_prestige = media.Movie("The prestige", 
+"Two stage magicians engage in competitive one-upmanship in an attempt to create the ultimate stage illusion.",
+"https://upload.wikimedia.org/wikipedia/en/d/d2/Prestige_poster.jpg",
+"https://www.youtube.com/watch?v=o4gHCmTQDVI")
+
+requiem_for_a_dream = media.Movie("Requiem For A Dream", 
+"The drug-induced utopias of four Coney Island people are shattered when their addictions run deep.",
+"https://upload.wikimedia.org/wikipedia/en/9/92/Requiem_for_a_dream.jpg",
+"https://www.youtube.com/watch?v=jzk-lmU4KZ4")
+
+eternal_sunshine_of_the_spotless_mind = media.Movie("Eternal Sunshine Of The Spotless Mind", 
+"A story of a guy who erasers his memory to ride himself from the pain of unsuccessful relationship",
+"https://upload.wikimedia.org/wikipedia/en/6/62/Eternal_sunshine_of_the_spotless_mind_ver3.jpg",
+"https://www.youtube.com/watch?v=yE-f1alkq9I")
+
+inglorious_bastards = media.Movie("Inglorious Bastards", 
+"Team of american souldier whoes mission is nothing less than to kil Hitler",
+"https://upload.wikimedia.org/wikipedia/en/c/c3/Inglourious_Basterds_poster.jpg",
+"https://www.youtube.com/watch?v=6AtLlVNsuAc")
+
+blade_runner = media.Movie("Blade Runner", 
+"A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.",
+"https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg",
+"https://www.youtube.com/watch?v=eogpIG53Cis")
+
+beautiful_mind = media.Movie("Beautiful Mind",
+"After John Nash, a brilliant but asocial mathematician, accepts secret work in cryptography, his life takes a turn for the nightmarish.",
+"https://upload.wikimedia.org/wikipedia/en/b/b8/A_Beautiful_Mind_Poster.jpg",
+"https://www.youtube.com/watch?v=YWwAOutgWBQ")
+
+v_for_vendetta=media.Movie("V For Vendetta",
+"In a future British tyranny, a shadowy freedom fighter, known only by the alias of 'v', plots to overthrow it with the help of a young woman.",
+"https://upload.wikimedia.org/wikipedia/en/9/9f/Vforvendettamov.jpg",
+"https://www.youtube.com/watch?v=KKvvOFIHs4k")
+
+mad_max_fury_road= media.Movie("Mad Max: Fury Road",
+"A woman rebels against a tyrannical ruler in postapocalyptic Australia in search for her home-land with the help of a group of female prisoners, a psychotic worshipper, and a drifter named Max.",
+"https://upload.wikimedia.org/wikipedia/en/6/6e/Mad_Max_Fury_Road.jpg",
+"https://www.youtube.com/watch?v=woHTUsl66BY")
+
+deadpool = media.Movie("Deadpool",
+"A former Special Forces operative turned mercenary is subjected to a rogue experiment that leaves him with accelerated healing powers, adopting the alter ego Deadpool.",
+"https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",
+"https://www.youtube.com/watch?v=oCvLUxICxEI")
+
+the_martian = media.Movie("The Martian",
+"An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.",
+"https://upload.wikimedia.org/wikipedia/en/c/cd/The_Martian_film_poster.jpg",
+"https://www.youtube.com/watch?v=ej3ioOneTy8")
+
+superbad = media.Movie("Superbad", 
+"Two co-dependent high school seniors are forced to deal with separation anxiety after their plan to stage a booze-soaked party goes awry.",
+"https://upload.wikimedia.org/wikipedia/en/8/8b/Superbad_Poster.png",
+"https://www.youtube.com/watch?v=MNpoTxeydiY")
+
+movies = [inceptions, avatar, the_departed, there_will_be_blood, life_is_beautiful, the_dark_knight, forest_gump, the_matrix, american_history_x, intersteller, the_prestige, requiem_for_a_dream, eternal_sunshine_of_the_spotless_mind, inglorious_bastards,
+blade_runner, superbad, beautiful_mind, v_for_vendetta, mad_max_fury_road, deadpool, the_martian]
+fresh_tomatoes.open_movies_page(movies)
