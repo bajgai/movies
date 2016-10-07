0938005d0325d0f909ae0b534c535f1df22eb049
Niranjan Bajgai <bajgainiranjan@gmail.com>
2016-10-06 21:25:51 -0600

    Doc string Added
    Added doc string

--- movie.py
+++ movie.py
@@ -1,5 +1,6 @@
 import webbrowser
 class Movie():
+	"""initializes instance of movie object"""
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
 		self.title = movie_title
 		self.storyline = movie_storyline
